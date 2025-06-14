666acb9492f5992c42d9bfce3a1452eb08f83b42
Patrick Zagiel <patrick.zagiel@gmail.com>
2025-06-14 13:15:57 +0200

    Initial Commit

---
+++ MultiTimer/MultiTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTimerApp.swift
+//  MultiTimer
+//
+//  Created by patrick zagiel on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
